95500c94ab5a30a7d64396e284843942f741d0bb
nknighta <n.knight.pc0627@gmail.com>
2023-10-31 21:27:08 +0900

    add multi size and shots url pattern

--- index.js
+++ index.js
@@ -1,39 +1,84 @@
+const THUMB_TYPES = [
+    /** w1280 */
+    'maxresdefault.jpg',
+    /** w640 */
+    'sddefault.jpg',
+    /** w480 */
+    'hqdefault.jpg',
+    /** w320 */
+    'mqdefault.jpg',
+    /** w120 */
+    'default.jpg',
+];
+
 function extractVideoId(url) {
+    // for normal url
     const pattern = /watch\?v=([a-zA-Z0-9_-]+)/;
     const match = url.match(pattern);
+    // for short url
     const patterns = "https://youtu.be/([a-zA-Z0-9_-]+)";
     const matchs = url.match(patterns);
-    const patternid = "([a-zA-Z0-9_-]+)";
+    // for only id
+    const patternid = "https://www.youtube.com/shorts/([a-zA-Z0-9_-]+)";
     const matchid = url.match(patternid);
+    // for shorts video url
+    
+    const patternshort = "shorts/([a-zA-Z0-9_-]+)";
+    const matchshort = url.match(patternshort);
     if (match && match[1]) {
         return match[1];
     } else if (matchs && matchs[1]) {
         return matchs[1];
     } else if (matchid && matchid[1]) {
         return matchid[1];
+    } else if (matchshort && matchshort[1]) {
+        return matchshort[1];
     }
     return null;
 }
 
-//console.log(videoId);
-
 function displayImage() {
-    var imageUrl = document.getElementById('imageUrl').value;
-    const videoId = extractVideoId(imageUrl);
-    const imageContainer = document.getElementById('imageContainer');
-    imageContainer.innerHTML = ''; // 既存の画像をクリア
-    try {
+    const getYtThumbnail = async (videoId) => {
+        // 画像をロードする処理
+        const loadImage = (src) => {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.onload = (e) => resolve(img);
+                img.src = src;
+            });
+        };
 
-        const img = document.createElement('img');
-        img.src = "https://i.ytimg.com/vi/" + videoId + "/maxresdefault.jpg";
-        img.alt = '画像';
-        imageContainer.appendChild(img);
-        img.style.width = '100%';
-    } catch (e) {
+        for (let i = 0; i < THUMB_TYPES.length; i++) {
+            const fileName = `https://img.youtube.com/vi/${videoId}/${THUMB_TYPES[i]}`;
+
+            const res = await loadImage(fileName);
+
+            // ダミー画像じゃなかったら（横幅が121px以上だったら）
+            // もしくは、これ以上小さい解像度が無かった場合は、このURLで決定
+            if (
+                !THUMB_TYPES[i + 1]
+                || (res).width > 120
+            ) {
+                return fileName;
+            }
+        }
+    };
+
+    (async () => {
+        // get vide id
+        var imageUrl = document.getElementById('imageUrl').value;
+        const videoId = extractVideoId(imageUrl);
+
+        const highQuality = await getYtThumbnail(videoId);
+        document.getElementById('highQuality').src = highQuality;
+
         const imageContainer = document.getElementById('imageContainer');
         imageContainer.innerHTML = ''; // 既存の画像をクリア
 
-        const img = document.createElement('h2');
-        img.textContent = 'URLが正しくありません';
-    }
+        const img = document.createElement('div');
+        //img.innerHTML = "https://i.ytimg.com/vi/" + videoId + "/maxresdefault.jpg";
+        img.innerHTML = `<a href="${highQuality}">thumbnail image</a>`;
+        imageContainer.appendChild(img);
+        console.log(highQuality);
+    })();
 }
